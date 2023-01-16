9968896dfbda522d3ee7d58a1f994f5224dc7416
paulhdev <paulohenriquerev@gmail.com>
2023-01-15 22:24:16 -0300

    Fix routes bottom tab and stack

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,21 +1,90 @@
 import React from 'react'
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import Home from '../screens/Home'
 import NewPost from '../screens/NewPost'
 import Search from '../screens/Search'
 import Profile from '../screens/Profile'
-// import UserPosts from '../screens/UserPosts'
+import UserPosts from '../screens/UserPosts'
 
 const Tab = createBottomTabNavigator()
+const Stack = createNativeStackNavigator()
+
+function StackRoutes() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Stack.Screen
+        name='Home'
+        component={Home}
+      />
+      <Stack.Screen
+        name='UserPosts'
+        component={UserPosts}
+      />
+    </Stack.Navigator>
+  )
+}
 
 export default function AppRoutes() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='Home' component={Home} />
-      <Tab.Screen name='NewPost' component={NewPost} />
-      <Tab.Screen name='Search' component={Search} />
-      <Tab.Screen name='Profile' component={Profile} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: '#eb5e11',
+        tabBarStyle: {
+          backgroundColor: '#fff',
+          borderTopWidth: 0
+        }
+      }}
+    >
+      <Tab.Screen
+        name='HomeTab'
+        component={StackRoutes}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name='home-outline' color={color} size={size} />
+          }
+        }}
+      />
+
+      <Tab.Screen
+        name='Search'
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name='search-outline' color={color} size={size} />
+          }
+        }}
+      />
+
+      <Tab.Screen
+        name='NewPost'
+        component={NewPost}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name='add-circle-outline' color={color} size={size} />
+          }
+        }}
+      />
+
+      <Tab.Screen
+        name='Profile'
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name='person-outline' color={color} size={size} />
+          }
+        }}
+      />
+
     </Tab.Navigator>
   )
 }
